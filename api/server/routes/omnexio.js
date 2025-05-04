0907dd22fd74c35c20206c55554596d1395e44c9
Vasil Derimanov <vasil.derimanov@gmail.com>
2025-05-04 17:28:14 +0300

    change subscription

--- api/server/routes/omnexio.js
+++ api/server/routes/omnexio.js
@@ -4,9 +4,11 @@ const omnexioBalanceController = require('../controllers/omnexio/OmnexioBalance'
 const omnexioSubscriptionCheckoutController = require('../controllers/omnexio/OmnexioSubscriptionCheckout');
 const { requireJwtAuth } = require('../middleware/');
 const omnexioSubscriptionPlans = require('~/server/controllers/omnexio/omnexioSubscriptionPlans');
+const omnexioSubscriptionChangeController = require('~/server/controllers/omnexio/OmnexioSubscriptionChange');
 
 router.get('/balance', requireJwtAuth, omnexioBalanceController);
 router.post('/subscriptions', requireJwtAuth, omnexioSubscriptionCheckoutController);
 router.get('/subscription-plans', requireJwtAuth, omnexioSubscriptionPlans);
+router.get('/subscription/change', requireJwtAuth, omnexioSubscriptionChangeController);
 
 module.exports = router;
--- packages/data-provider/src/data-service.ts
+++ packages/data-provider/src/data-service.ts
@@ -9,6 +9,7 @@ import * as config from './config';
 import request from './request';
 import * as s from './schemas';
 import * as r from './roles';
+import { omnexioSubscriptionChange } from './api-endpoints';
 
 export function abortRequestWithMessage(
   endpoint: string,
@@ -110,6 +111,13 @@ export function createOmnexioSubscription(subscriptionPlanId: number): Promise<s
 
 export function getOmnexioSubscriptionPlans(): Promise<string> {
   return request.get(endpoints.omnexioSubscriptionPlans());
+}
+
+export function changeOmnexioSubscription(subscriptionPlanId: number): Promise<void> {
+  const payload = {
+    subscription_plan_id: subscriptionPlanId,
+  };
+  return request.post(endpoints.omnexioSubscriptionChange(), payload);
 }
 
 export const updateTokenCount = (text: string) => {
