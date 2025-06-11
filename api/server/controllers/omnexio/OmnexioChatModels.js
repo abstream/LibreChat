af002a1a405e8d29c9ad0c7d72d4f0fa71306e5f
Vasil Derimanov <vasil.derimanov@gmail.com>
2025-06-12 01:34:53 +0300

    omnexio endpoint

--- api/server/controllers/omnexio/OmnexioChatModels.js
+++ api/server/controllers/omnexio/OmnexioChatModels.js
@@ -5,11 +5,20 @@ async function omnexioChatModels(req, res) {
   const { OMNEXIO_BASE_URL, OMNEXIO_API_KEY } = process.env;
   const url = `${OMNEXIO_BASE_URL}/v1/chat-models`;
 
-  const response = await axios.get(url, {
+  const config = {
     headers: {
       Authorization: `Bearer ${OMNEXIO_API_KEY}`,
     },
-  });
+  };
+
+  // Only add query params if model is provided
+  if (req.query.model) {
+    config.params = {
+      label: req.query.model,
+    };
+  }
+
+  const response = await axios.get(url, config);
 
   return res.status(200).send(response.data);
 }
