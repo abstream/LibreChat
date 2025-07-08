30f588b3fd5f4de8bf8acdcdf1c779488f828485
Vasil Derimanov <vasil.derimanov@gmail.com>
2025-07-09 00:29:11 +0300

    social redirect

--- client/src/routes/Logged.tsx
+++ client/src/routes/Logged.tsx
@@ -1,9 +1,4 @@
-import { useNavigate } from 'react-router-dom';
-import ResetPassword from '../components/Auth/ResetPassword';
-
 function Logged() {
-  const navigate = useNavigate();
-
   let redirect = '/c/new';
   const intendedRedirect = sessionStorage.getItem('intendedRedirect');
   if (intendedRedirect && intendedRedirect !== '/logged') {
