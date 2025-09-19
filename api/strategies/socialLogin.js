78ee18356962855f1878ee180366d67ff0846205
Vasil Derimanov <vasil.derimanov@gmail.com>
2025-09-19 14:00:44 +0300

    authorize social user even with another provider

--- api/strategies/socialLogin.js
+++ api/strategies/socialLogin.js
@@ -28,17 +28,9 @@ const socialLogin =
 
       const existingUser = await findUser({ email: email.trim() });
 
-      if (existingUser?.provider === provider) {
+      if (existingUser) {
         await handleExistingUser(existingUser, avatarUrl, appConfig);
         return cb(null, existingUser);
-      } else if (existingUser) {
-        logger.info(
-          `[${provider}Login] User ${email} already exists with provider ${existingUser.provider}`,
-        );
-        const error = new Error(ErrorTypes.AUTH_FAILED);
-        error.code = ErrorTypes.AUTH_FAILED;
-        error.provider = existingUser.provider;
-        return cb(error);
       }
 
       const ALLOW_SOCIAL_REGISTRATION = isEnabled(process.env.ALLOW_SOCIAL_REGISTRATION);
